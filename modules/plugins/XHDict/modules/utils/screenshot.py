50377322baf99333fe687c9ff4eb93f48e3082cc
Luoyao <yuejieyao@gmail.com>
2021-09-03 16:32:11 +0800

    update screenshot
    增加广告屏蔽插件

--- modules/plugins/XHDict/modules/utils/screenshot.py
+++ modules/plugins/XHDict/modules/utils/screenshot.py
@@ -1,15 +1,22 @@
-import base64
 from playwright.sync_api import sync_playwright
+
+path_to_extension = "modules/resource/chrome_extension"
+user_data_dir = "modules/resource/chrome_user_data"
 
 
 def getScreenshot(url: str, path: str):
     with sync_playwright() as p:
-        bowser_type = p.chromium
-        bowser = bowser_type.launch()
+        bowser = p.chromium.launch_persistent_context(
+            user_data_dir,
+            headless=True,
+            args=[
+                f"--disable-extensions-except={path_to_extension}",
+                f"--load-extension={path_to_extension}",
+            ],
+        )
         page = bowser.new_page()
         page.goto(url, wait_until='load', timeout=10000)
         page.set_viewport_size({'width': 1600, 'height': 2000})
         card = page.query_selector(".shiyi_content")
         assert card is not None
         card.screenshot(path=path)
-        bowser.close()
