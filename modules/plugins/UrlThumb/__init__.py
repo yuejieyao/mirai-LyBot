438f6302a4caabb136abe67a403e7f0adfc0d24f
Luoyao <yuejieyao@gmail.com>
2022-05-05 09:22:41 +0800

    update urlthumb
    由于大量网址无法正常通过outline获取到信息,取消了outline的功能

--- modules/plugins/UrlThumb/__init__.py
+++ modules/plugins/UrlThumb/__init__.py
@@ -6,20 +6,18 @@
 @Author      :yuejieyao
 @version      :1.0
 '''
-
-from ..miraiPlugin import MiraiMessagePluginProcessor
-from modules.message.messageChain import MessageChain
-from modules.message.messageType import Plain, Image, App, Xml
-from modules.http.miraiMessageRequest import MiraiMessageRequest as MsgReq
-from .modules.utils.drawBilibiliImg import binfo_image_create
 import requests
 import re
 import os
 from lxml import etree
 from PIL import Image as IMG
 from io import BytesIO
-import time
 import json
+from ..miraiPlugin import MiraiMessagePluginProcessor
+from modules.message.messageChain import MessageChain
+from modules.message.messageType import Plain, Image, App, Xml
+from modules.http.miraiMessageRequest import MiraiMessageRequest as MsgReq
+from .modules.utils.drawBilibiliImg import binfo_image_create
 
 
 @MiraiMessagePluginProcessor.mirai_group_message_plugin_register('UrlThumb')
@@ -31,54 +29,57 @@ class UrlThumb:
     temp_path = os.path.join(os.path.dirname(__file__), 'cache')
 
     def process(self, chains: MessageChain, group: int, target: int,  quote: int):
-        urls = []
+        url = ""
         if chains.has(App):
             json_msg = json.loads(chains.get(App)[0].content)
             try:
                 desc = json_msg['desc']
                 if desc == '新闻':
-                    urls.append(json_msg['meta']['news']['jumpUrl'])
+                    url = json_msg['meta']['news']['jumpUrl']
+                elif desc == '哔哩哔哩':
+                    url = json_msg['meta']['detail_1']['qqdocurl']
             except KeyError:
                 pass
-            else:
-                urls.append(json_msg['meta']['detail_1']['qqdocurl'])
-        if chains.has(Xml):
+            if url is None:
+                url = json_msg['meta']['detail_1']['qqdocurl']
+        elif chains.has(Xml):
             xml_msg = etree.fromstring(chains.get(Xml)[0].xml.encode('utf-8'))
             try:
-                urls.append(xml_msg.xpath('/msg/@url')[0])
+                url = xml_msg.xpath('/msg/@url')[0]
             except IndexError:
                 pass
             else:
                 pass
         else:
-            urls.extend(re.findall(
-                r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', chains.asDisplay()))
-        if len(urls) > 0:
-            for url in urls:
-                if "b23.tv" in url:
-                    url = self.b23_extract(url)
-                p = re.compile(r'av(\d{1,12})|BV(1[A-Za-z0-9]{2}4.1.7[A-Za-z0-9]{2})')
-                video_number = p.search(url)
+            try:
+                url = re.findall(
+                    r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', chains.asDisplay())[0]
+            except:
+                pass
+        if url:
+            if "b23.tv" in url:
+                url = self.b23_extract(url)
+            p = re.compile(r'av(\d{1,12})|BV(1[A-Za-z0-9]{2}4.1.7[A-Za-z0-9]{2})')
+            video_number = p.search(url)
+            if video_number:
+                video_number = video_number.group(0)
                 if video_number:
-                    video_number = video_number.group(0)
-                    if video_number:
-                        video_info = self.video_info_get(video_number)
-                        if video_info:
-                            if video_info["code"] != 0:
-                                MsgReq().sendGroupMessage(msg=MessageChain(
-                                    [Plain(text=f'解析到B站视频:{video_number},但视频不存在')]), target=group)
-                            else:
-                                try:
-                                    save_path = os.path.join(self.temp_path, 'bilibili_thumb.jpg')
-                                    if binfo_image_create(video_info=video_info, save_path=save_path):
-                                        MsgReq().sendGroupMessage(msg=MessageChain(
-                                            [Image(image_type='group', file_path=save_path)]), target=group)
-                                except Exception:
+                    video_info = self.video_info_get(video_number)
+                    if video_info:
+                        if video_info["code"] != 0:
+                            MsgReq().sendGroupMessage(msg=MessageChain(
+                                [Plain(text=f'解析到B站视频:{video_number},但视频不存在')]), target=group)
+                        else:
+                            try:
+                                save_path = os.path.join(self.temp_path, 'bilibili_thumb.jpg')
+                                if binfo_image_create(video_info=video_info, save_path=save_path):
                                     MsgReq().sendGroupMessage(msg=MessageChain(
-                                        [Plain(text=f"B站视频{video_number}解析失败")]), target=group)
-                else:
-                    MsgReq().sendGroupMessage(msg=self.outline(url), target=group)
-                time.sleep(1)  # 防调用频率过快
+                                        [Image(image_type='group', file_path=save_path)]), target=group)
+                            except Exception:
+                                MsgReq().sendGroupMessage(msg=MessageChain(
+                                    [Plain(text=f"B站视频{video_number}解析失败")]), target=group)
+            else:
+                MsgReq().sendGroupMessage(msg=MessageChain([Plain(text=url)]), target=group)
 
     def outline(self, url: str) -> MessageChain:
         api_url = f"https://api.outline.com/v3/parse_article?source_url=%s" % url
