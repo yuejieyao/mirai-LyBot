a8d021f4cc66c74061da4eba1db10f75b0f7964a
Luoyao <yuejieyao@gmail.com>
2022-10-20 13:56:39 +0800

    update NovelAI

--- modules/plugins/NovelAI/__init__.py
+++ modules/plugins/NovelAI/__init__.py
@@ -18,10 +18,58 @@ import base64
 from modules.http.miraiMessageRequest import MiraiMessageRequest as MsgReq
 from modules.message.messageChain import MessageChain
 from modules.message.messageType import Plain, Image
-from .modules.utils.novelAIUtils import NovelAIUtils
+from .modules.utils.novelAIUtils import NovelAIUtils, SamplerType
 from ..miraiPlugin import MiraiMessagePluginProcessor
 from modules.utils import log
 from modules.message.miraiMessageMonitorHandler import MiraiMessageMonitor, MiraiMessageMonitorHandler
+
+
+def getParam(command: str, name: str, split=' '):
+    i = command.find(name) + len(name) + 1
+    j = command.find(split, i)
+    return command[i:j]
+
+
+def getKwargs(command: str):
+    if '-prompt' not in command:
+        raise Exception("命令格式不正确,请使用:-prompt tag1,tag2,tag3 来增加正面提示tag")
+    kwargs = {}
+    if '-seed' in command:
+        try:
+            kwargs['seed'] = int(getParam(command, '-seed'))
+        except:
+            raise Exception("seed格式不正确,请使用:-seed 1234567 来设定图片的种子")
+    if '-step' in command:
+        try:
+            kwargs['steps'] = int(getParam(command, '-step'))
+        except:
+            raise Exception("step格式不正确,请使用:-step 28 来设定AI生成图片使用的步数")
+    if '-size' in command:
+        size_str = getParam(command, '-size')
+        size_split = re.split('[xX*]', size_str)
+        if len(size_split) != 2:
+            raise Exception("size格式不正确,请使用大写或小写的字母X,或*来分割宽高,如: -size 512*768")
+        else:
+            width, height = NovelAIUtils.getValidSize((int(size_split[0]), int(size_split[1])))
+            kwargs['width'] = width
+            kwargs['height'] = height
+    if '-scale' in command:
+        try:
+            kwargs['scale'] = float(getParam(command, '-scale'))
+        except:
+            raise Exception("scale格式不正确,请使用: -scale 12 来设定AI的服从度")
+    if '-sampler' in command:
+        try:
+            n = int(getParam(command, '-sampler'))
+            kwargs['sampler'] = SamplerType(n).name
+        except:
+            raise Exception('sampler参数不正确,可选值为1-3,分别对应k_euler_ancestral,k_euler和k_lms三种采样模式')
+    if '-uc' in command:
+        kwargs['uc'] = getParam(command, '-uc', '-')
+    kwargs['keywords'] = command[command.find('-prompt') + 8:]
+    if NovelAIUtils.hasZHChar(kwargs['keywords']):
+        raise Exception('prompt参数不正确,请不要包含中文')
+    return kwargs
 
 
 @MiraiMessagePluginProcessor.mirai_group_message_plugin_register('NovelAI')
@@ -33,46 +81,49 @@ class NovelAI:
 
     def process(self, chains: MessageChain, group: int, target: int, quote: int):
         msg_display = chains.asDisplay()
-        if re.match('作图 .*', msg_display) is not None:
-            if ' -s ' in msg_display:
-                i = msg_display.find('-s') + 3
-                j = msg_display.find(' ', i)
-                seed = int(msg_display[i:j])
-                keywords = msg_display[j + 1:]
+        if msg_display in ['ci -h', 'cii -h']:
+            msg = MessageChain([Plain('基础命令: ci -prompt tag1,tag2,tag3,tag4 \n'),
+                                Plain('可在ci后,-prompt前增加附加指令: \n'),
+                                Plain('    使用固定的种子,默认随机: -seed 1234567\n'),
+                                Plain('    使用设定的步数,默认28,上传图片固定50: -step 28\n'),
+                                Plain('    生成自定义大小的图片,默认512*768: -size 512x768\n'),
+                                Plain('    设定AI的服从度,默认12: -scale 12\n'),
+                                Plain('    使用其他采样模式,默认为1,可选值1-3分别对应k_euler_ancestral,k_euler和k_lms : -sampler 1\n'),
+                                Plain('    使用自定义的负面tag,默认为官网设定的自带tag: -uc tag1,tag2,tag3\n')
+                                ])
+            MsgReq().sendGroupMessage(msg=msg, target=group)
+        elif msg_display == 'ci -m':
+            MsgReq().sendGroupMessage(
+                MessageChain([Plain('ci -step 28 -size 512*768 -scale 12 -sampler 1 -prompt tag')]),
+                target=group)
+        elif re.match('ci .*', msg_display) is not None:
+            msg_req = MsgReq()
+            try:
+                kwargs = getKwargs(msg_display[3:])
+            except Exception as e:
+                msg_req.sendGroupMessage(msg=MessageChain([Plain(text=str(e))]), target=group, quote=quote)
+                return
 
-            else:
-                msg_split = msg_display.split(' ', 1)
-                keywords = msg_split[1]
-                keywords = keywords.replace('，', ',')
-                seed = round(time.time() / 10) + random.randint(1, 99)
+            msg_req.sendGroupMessage(msg=MessageChain([Plain(text="正在请求绘制...")]), target=group)
+            fname = f"{uuid.uuid1()}.png"
+            path = os.path.join(self.directory, fname)
+            try:
+                if seed := NovelAIUtils().createImg(path=self.directory, fname=fname, **kwargs):
+                    log.info('[Plugins][NovelAI] create Img success, keywords=' + kwargs['keywords'])
+                    msg = MessageChain([Plain(text="seed: %s\n" % seed)])
+                    img = Image(image_type='group', file_path=path)
+                    if img.image_id:
+                        msg.append(img)
+                        msg_req.sendGroupMessage(msg=msg, target=group)
+            except:
+                log.error(msg=traceback.format_exc())
+        elif re.match('cii .*', msg_display) is not None:
+            try:
+                kwargs = getKwargs(msg_display[3:])
+            except Exception as e:
+                MsgReq().sendGroupMessage(msg=MessageChain([Plain(text=str(e))]), target=group, quote=quote)
+                return
 
-            if not NovelAIUtils.hasZHChar(keywords):
-                msg_req = MsgReq()
-                msg_req.sendGroupMessage(msg=MessageChain([Plain(text="正在请求绘制...")]), target=group)
-                fname = f"{uuid.uuid1()}.png"
-                path = os.path.join(self.directory, fname)
-                try:
-                    if NovelAIUtils().createImg(seed, keywords, self.directory, fname):
-                        log.info('[Plugins][NovelAI] create Img success, keywords=' + keywords)
-                        msg = MessageChain([Plain(text="seed: %s\n" % seed)])
-                        img = Image(image_type='group', file_path=path)
-                        if img.image_id:
-                            msg.append(img)
-                            msg_req.sendGroupMessage(msg=msg, target=group)
-                except:
-                    log.error(msg=traceback.format_exc())
-        elif re.match('以图作图 .*', msg_display) is not None:
-            if ' -s ' in msg_display:
-                i = msg_display.find('-s') + 3
-                j = msg_display.find(' ', i)
-                seed = int(msg_display[i:j])
-                keywords = msg_display[j + 1:]
-
-            else:
-                msg_split = msg_display.split(' ', 1)
-                keywords = msg_split[1]
-                keywords = keywords.replace('，', ',')
-                seed = round(time.time() / 10) + random.randint(1, 99)
             fname = f"{uuid.uuid1()}.png"
             path = os.path.join(self.directory, fname)
 
@@ -88,14 +139,16 @@ class NovelAI:
                     _img = _msg.get(Image)[0]
                     res = requests.get(_img.chain['url']).content
                     base64_data = base64.b64encode(res)
-                    if NovelAIUtils().createImgByImg(base64_data, seed, keywords, self.directory, fname):
+                    _msg_req = MsgReq()
+                    _msg_req.sendGroupMessage(msg=MessageChain([Plain(text="正在请求绘制...")]), target=_group)
+                    if _seed := NovelAIUtils().createImg(path=self.directory, fname=fname, img=base64_data, **kwargs):
                         log.info('[Plugins][NovelAI] create Img success,base_img= %s ,keywords= %s' % (
-                            _img.chain['url'], keywords))
-                        __msg = MessageChain([Plain(text="seed: %s\n" % seed)])
+                            _img.chain['url'], kwargs['keywords']))
+                        __msg = MessageChain([Plain(text="seed: %s\n" % _seed)])
                         __img = Image(image_type='group', file_path=path)
                         if __img.image_id:
                             __msg.append(__img)
-                            MsgReq().sendGroupMessage(msg=__msg, target=_group)
+                            _msg_req.sendGroupMessage(msg=__msg, target=_group)
                 except:
                     log.error(msg=traceback.format_exc())
 
--- modules/plugins/NovelAI/modules/utils/novelAIUtils.py
+++ modules/plugins/NovelAI/modules/utils/novelAIUtils.py
@@ -9,10 +9,23 @@
 import base64
 import os.path
 import requests
+import time
+import random
+from typing import Tuple
+from enum import Enum
+
 from modules.conf import config
 
 
+class SamplerType(Enum):
+    k_euler_ancestral = 1
+    k_euler = 2
+    k_lms = 3
+
+
 class NovelAIUtils:
+    MAX_OUTPUT_PIXELS = 1048576
+    DEFAULT_UC = "lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry"
 
     def __init__(self):
         self.url = "https://api.novelai.net"
@@ -24,56 +37,68 @@ class NovelAIUtils:
                         "referer": 'https://novelai.net/',
                         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36', }
 
-    def createImg(self, seed: int, keywords: str, path: str, fname: str):
-        data = {
-            "input": "masterpiece, best quality, " + keywords,
-            "model": "safe-diffusion",
-            "parameters": {
-                "width": 512,
-                "height": 768,
-                "scale": 12,
-                "sampler": "k_euler_ancestral",
-                "steps": 28,
-                "seed": seed,
-                "n_samples": 1,
-                "ucPreset": 0,
-                "qualityToggle": True,
-                "uc": "lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry"
-            }
-        }
-        resp = requests.post(self.url + '/ai/generate-image', json=data, headers=self.headers)
-        resp.raise_for_status()
-        res = resp.text[27:]
-        with open(os.path.join(path, fname), 'wb') as out_file:
-            out_file.write(base64.b64decode(res))
-        return True
+    def createImg(self, keywords: str, path: str, fname: str, width: int = 512, height: int = 768,
+                  scale: float = 12, steps: int = 28, sampler: str = SamplerType(1).name, uc: str = DEFAULT_UC,
+                  seed: int = None, img: bytes = None):
+        if not seed:
+            seed = round(time.time() / 10) + random.randint(1, 99)
 
-    def createImgByImg(self, img: bytes, seed: int, keywords: str, path: str, fname: str):
         data = {
             "input": "masterpiece, best quality, " + keywords,
             "model": "safe-diffusion",
             "parameters": {
-                "image": img.decode('ascii'),
-                "width": 512,
-                "height": 768,
-                "scale": 11,
-                "sampler": "k_euler_ancestral",
-                "steps": 50,
+                "width": width,
+                "height": height,
+                "scale": scale,
+                "sampler": sampler,
+                "steps": steps,
                 "seed": seed,
                 "n_samples": 1,
                 "ucPreset": 0,
-                "strength": 0.7,
-                "noise": 0.2,
                 "qualityToggle": True,
-                "uc": "lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry"
+                "uc": uc
             }
         }
+
+        if img:
+            data["parameters"]["image"] = img.decode('ascii')
+            data["parameters"]["steps"] = 50
+            data["parameters"]["strength"] = 0.7
+            data["parameters"]["noise"] = 0.2
+
         resp = requests.post(self.url + '/ai/generate-image', json=data, headers=self.headers)
         resp.raise_for_status()
         res = resp.text[27:]
         with open(os.path.join(path, fname), 'wb') as out_file:
             out_file.write(base64.b64decode(res))
-        return True
+        return seed
+
+    # def createImgByImg(self, img: bytes, seed: int, keywords: str, path: str, fname: str):
+    #     data = {
+    #         "input": "masterpiece, best quality, " + keywords,
+    #         "model": "safe-diffusion",
+    #         "parameters": {
+    #             "image": img.decode('ascii'),
+    #             "width": 512,
+    #             "height": 768,
+    #             "scale": 11,
+    #             "sampler": "k_euler_ancestral",
+    #             "steps": 50,
+    #             "seed": seed,
+    #             "n_samples": 1,
+    #             "ucPreset": 0,
+    #             "strength": 0.7,
+    #             "noise": 0.2,
+    #             "qualityToggle": True,
+    #             "uc": "lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry"
+    #         }
+    #     }
+    #     resp = requests.post(self.url + '/ai/generate-image', json=data, headers=self.headers)
+    #     resp.raise_for_status()
+    #     res = resp.text[27:]
+    #     with open(os.path.join(path, fname), 'wb') as out_file:
+    #         out_file.write(base64.b64decode(res))
+    #     return True
 
     @staticmethod
     def hasZHChar(chars: str):
@@ -81,3 +106,22 @@ class NovelAIUtils:
             if u'\u4e00' <= char <= u'\u9fa5':
                 return True
         return False
+
+    @staticmethod
+    def isSizeValid(size: Tuple[int, int]):
+        width, height = size
+        if width % 64 == 0 and height % 64 == 0 and width * height <= NovelAIUtils.MAX_OUTPUT_PIXELS:
+            return True
+        return False
+
+    @staticmethod
+    def getValidSize(size: Tuple[int, int]):
+        width, height = size
+        if width % 64 > 0:
+            width -= width % 64
+        if height % 64 > 0:
+            height -= height % 64
+        while width * height > NovelAIUtils.MAX_OUTPUT_PIXELS:
+            width -= 64
+            height -= 64
+        return width, height
